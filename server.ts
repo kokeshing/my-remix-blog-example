bf198f2737df370578a65e0442c0780776290886
Kotaro Onishi <kou7cdb1a@gmail.com>
2024-01-17 01:51:45 +0900

    server.tsの書き換えが抜けていたので修正

--- server.ts
+++ server.ts
@@ -8,6 +8,6 @@ if (process.env.NODE_ENV === 'development') {
 
 export const onRequest = createPagesFunctionHandler({
   build,
-  getLoadContext: (context) => ({ env: context.env }),
+  getLoadContext: (context) => ({ env: context.env, ctx: context }),
   mode: build.mode,
 })
